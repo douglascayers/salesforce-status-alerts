c08038b7f8ae9f7e9c3822e5b4ad44fa3de1d3ce
Doug Ayers <douglascayers@gmail.com>
2016-10-22 01:14:08 -0500

    updated email addon

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-var http = require('http');
 var request = require('request');
-// var schedule = require('node-schedule');
-// var express = require('express');
-// var app = express();
 
-// app.set('port', (process.env.PORT || 5000));
-
-// app.get( '/', function( request, response ) {
-
-//     return http.get({
-//         host: 'http://api.status.salesforce.com',
-//         path: '/v1/instances/na5/status',
-//         json: true
-//     }, function( response ) {
-//         console.log( response );
-//         return response;
-//     });
-
-// });
-
-// app.listen( app.get('port'), function() {
-//     console.log( 'Node app is running on port', app.get('port') );
-// });
-
-var request = require('request');
 var instanceName = 'na5';
+
 var options = {
     url : 'http://api.status.salesforce.com/v1/instances/' + instanceName + '/status',
     json : true
 };
+
 console.log('Executing request, options=' + JSON.stringify( options ) );
+
 request( options , function( error, response, body ) {
+
     console.log( body );
+
+    sendEmail({
+        "from": "donotreply@example.com",
+        "to": "douglascayers@gmail.com",
+        "subject": "Test",
+        "textBody": "Test Message"
+    });
+
 });
+
+/**
+ * options = {
+ *   'from'    : 'you@email.com',
+ *   'to'      : 'someone@email.com',
+ *   'subject' : 'hello world',
+ *   'textBody': 'your text message'
+ * }
+ */
+function sendEmail( options ) {
+
+    // https://devcenter.heroku.com/articles/sendgrid#node-js
+
+    var helper = require('sendgrid').mail;
+    var from_email = new helper.Email( options.from );
+    var to_email = new helper.Email( options.to );
+    var subject = options.subject;
+    var content = new helper.Content('text/plain', options.textBody );
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
+    });
+
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
+    });
+
+}
